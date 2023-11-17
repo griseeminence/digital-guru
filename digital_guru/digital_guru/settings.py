e0d5cb8041b4a1b12f693af6906dd7be61c442f5
griseeminence <p.kolt080@gmail.com>
2023-11-17 21:36:29 +0800

    before allauth templates

--- digital_guru/digital_guru/settings.py
+++ digital_guru/digital_guru/settings.py
@@ -52,7 +52,7 @@ ROOT_URLCONF = 'digital_guru.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
